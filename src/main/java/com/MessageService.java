428454bc13effe6dca52b938be9bc3328159d5af
Shubham Singh Kushwah <shubhamkushwah123@gmail.com>
2020-04-28 10:44:57 +0530

    My First Commit for DevOps Bootcamp for Spring Boot Application

---
+++ src/main/java/com/MessageService.java
@@ -0,0 +1,15 @@
+package com;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageService {
+	
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Hello Engineers";
+	}
+	
+
+}
